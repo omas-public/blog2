151815cb3aae6be6cd14227127763a7826589fc8
Omas Naohiko <omas@std.it-college.ac.jp>
2023-12-20 15:01:50 +0900

    chapter 4.3 finished
    HeroレイアウトをSideBySideCenterに整える

--- components/hero.js
+++ components/hero.js
@@ -1,12 +1,12 @@
 import styles from 'styles/hero.module.css'
 const Hero = ({ title, subtitle, imageOn = false }) => (
-  <>
+  <div className={styles.flexContainer}>
     <div className={styles.text}>
       <h1 className={styles.title}>{title}</h1>
       <p className={styles.subtitle}>{subtitle}</p>
     </div>
     {imageOn && <figure> [image] </figure>}
-  </>
+  </div>
 )
 
 export default Hero
