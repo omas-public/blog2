47dec6b9e2c55d4dac3b2c11649246f68ccfab2f
Omas Naohiko <omas@std.it-college.ac.jp>
2023-12-20 16:04:54 +0900

    chapter 4.7 finished
    Contactコンポーネントを作成してAboutページに追加

---
+++ components/contact.js
@@ -0,0 +1,10 @@
+import styles from 'styles/contact.module.css'
+
+const Contact = () => (
+  <div className={styles.stack}>
+    <h3 className={styles.heading}>Contact</h3>
+    <address>cube@web.mail.address</address>
+  </div>
+)
+
+export default Contact
--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,8 @@
 import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
+import Contact from 'components/contact'
+
 const props = { title: 'About', subtitle: 'About development activities' }
 const About = () => (
   <Container>
@@ -32,6 +34,7 @@ const About = () => (
         公開リポジトリを活用すると、全世界のどこからでもアクセスし、開発者が関連するプロジェクトのタスクを利用することができます。
       </p>
     </PostBody>
+    <Contact />
   </Container>
 )
 
