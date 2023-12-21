aaaf313bda61dc9ef29ddaf1b045792e3f062d2e
Omas Naohiko <omas@std.it-college.ac.jp>
2023-12-21 16:09:49 +0900

    chapter 5.7 finished
    font awesomeのアイコンをインストールして使用できる状態にする

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import Layout from 'components/layout'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoCss = false
 
 export default function App ({ Component, pageProps }) {
   return (
