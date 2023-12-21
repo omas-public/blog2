cd0fce4b02c5de8aff6d30767af2add1de45675f
Omas Naohiko <omas@std.it-college.ac.jp>
2023-12-21 16:01:18 +0900

    chapter 5.6 finished
    Heroコンポーネントに画像を追加してBlurとPriorityを有効にする

--- components/hero.js
+++ components/hero.js
@@ -1,11 +1,25 @@
 import styles from 'styles/hero.module.css'
+import Image from 'next/legacy/image'
+import cube from 'images/cube.jpg'
+
 const Hero = ({ title, subtitle, imageOn = false }) => (
   <div className={styles.flexContainer}>
     <div className={styles.text}>
       <h1 className={styles.title}>{title}</h1>
       <p className={styles.subtitle}>{subtitle}</p>
     </div>
-    {imageOn && <figure> [image] </figure>}
+    {imageOn && (
+      <figure className={styles.image}>
+        <Image
+          src={cube}
+          layout='responsive'
+          alt=''
+          sizes='(min-width: 1152px) 1152px, (min-width:768px) 50vw, 100vw'
+          placeholder='blur'
+          priority
+        />
+      </figure>
+    )}
   </div>
 )
 
