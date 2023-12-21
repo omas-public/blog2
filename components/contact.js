ded85f835ba50fb5a413f59ae9b797c11dbdaa9d
Omas Naohiko <omas@std.it-college.ac.jp>
2023-12-21 16:36:49 +0900

    chapter 5.9 finished
    Socialコンポーネントを作成してコンタクトとフッターに配置

--- components/contact.js
+++ components/contact.js
@@ -1,8 +1,10 @@
 import styles from 'styles/contact.module.css'
+import Social from 'components/social'
 
 const Contact = () => (
   <div className={styles.stack}>
     <h3 className={styles.heading}>Contact</h3>
+    <Social />
     <address>cube@web.mail.address</address>
   </div>
 )
--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,13 @@
 import Container from 'components/container'
 import Logo from 'components/logo'
+import Social from 'components/social'
 import styles from 'styles/footer.module.css'
 const Footer = () => (
   <footer className={styles.wrapper}>
     <Container>
       <div className={styles.flexContainer}>
         <Logo />
+        <Social />
       </div>
     </Container>
   </footer>
