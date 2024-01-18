b1bd865185729b7abcf21e1f9a209609a35c1412
Omas Naohiko <omas@std.it-college.ac.jp>
2024-01-18 16:26:04 +0900

    fix two-colomn

--- components/two-column.js
+++ components/two-column.js
@@ -1,10 +1,14 @@
 import styles from 'styles/two-column.module.css'
 
 const TwoColumn = ({ children }) => (
-  <div class={styles.flexContainer}>{children}</div>
+  <div className={styles.flexContainer}>{children}</div>
 )
 
-const TwoColumnMain = ({ children }) => <div className={styles.main}>{children}</div>
-const TwoColumnSidebar = ({ children }) => <div className={styles.sidebar}>{children}</div>
+const TwoColumnMain = ({ children }) => (
+  <div className={styles.main}>{children}</div>
+)
+const TwoColumnSidebar = ({ children }) => (
+  <div className={styles.sidebar}>{children}</div>
+)
 
 export { TwoColumn, TwoColumnMain, TwoColumnSidebar }
